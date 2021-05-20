c2b9974dbfce62c87b910c7ec9007e869dbe831e
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-05-20 22:48:26 +0400

    feat: add select component

--- src/components/Select/CitySelect.tsx
+++ src/components/Select/CitySelect.tsx
@@ -1,27 +1,49 @@
 import React, { useState } from 'react';
+import SelectArrowBottom from '../../assets/images/icons/chevron-bottom.png';
+import SelectArrowTop from '../../assets/images/icons/chevron-top.png';
 
 interface IProps {
   getCity: (city: string) => void;
 }
 
 const CitySelect: React.FC<IProps> = ({ getCity }) => {
-  const [city, setCity] = useState<string>('');
+  const [city, setCity] = useState<string>('Select city');
   const cities = ['Samara', 'Tolyatti', 'Saratov', 'Kazan', 'Krasnodar'];
-  const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-    setCity(event.target.value);
-    getCity(event.target.value);
+  const [toggleSelect, setToggleSelect] = useState<boolean>(false);
+
+  const toggleOptGroup = (event: any) => {
+    setToggleSelect(!toggleSelect);
+    event.stopPropagation();
   };
 
+  const selectCity = (event: React.MouseEvent<HTMLLIElement>) => {
+    const target: any = event.target;
+    setCity(target.innerText);
+    getCity(target.innerText);
+  };
+
+  window.addEventListener('click', (event: any) => {
+    if (toggleSelect) {
+      toggleOptGroup(event);
+    }
+  });
+
   return (
-    <div className='city-select'>
-      <select className='city-select__select' value={city} onChange={handleChange}>
-        <option value='' disabled selected hidden>Select sity</option>
-        {
-          cities.map((city) => {
-            return <option className='city-select__select__option' key={city} value={city}>{city}</option>;
-          })
-        }
-      </select>
+    <div className='city-select' id='select' onClick={toggleOptGroup}>
+      <div className='city-select__text'>{city}</div>
+      <div className='city-select__arrow'>
+        <img src={toggleSelect ? SelectArrowTop : SelectArrowBottom} alt='select arrow' />
+      </div>
+      {
+        toggleSelect ?
+          <ul className='city-select__optgroup'>
+            {
+              cities.map((city) => {
+                return <li id={city} className='city-select__select-item' key={city} value={city} onClick={selectCity}>{city}</li>;
+              })
+            }
+          </ul> : null
+      }
     </div >
   );
 };
