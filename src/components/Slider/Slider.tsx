4e2024e80828ed93967b54ea866eb9a03adf1539
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-05-21 17:35:57 +0400

    feat: add enabled/disabled arrows for the slider component

--- src/components/Slider/Slider.tsx
+++ src/components/Slider/Slider.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import arrow from '../../assets/images/icons/chevron-left.png';
+import arrowActive from '../../assets/images/icons/chevron-left-active.png';
+import arrowDisabled from '../../assets/images/icons/chevron-left-disabled.png';
 import { COUNT_CARDS_WEB, COUNT_WEATHER_CARDS_ALL } from '../../common/components/constants';
 import WeatherCard from '../WeatherCard/WeatherCard';
 import { IWeatherDetailsList } from '../../common/interfaces/interfaces';
@@ -14,11 +15,15 @@ const Slider: React.FC<IProps> = ({ weatherData }) => {
   const maxUpdateCardCurent = COUNT_WEATHER_CARDS_ALL - COUNT_CARDS_WEB;
 
   const handleClickPrev = () => {
-    setCurrentStartItem(currentStartItem - 1);
+    if (currentStartItem !== 0) {
+      setCurrentStartItem(currentStartItem - 1);
+    }
   };
 
   const handleClickNext = () => {
-    setCurrentStartItem(currentStartItem + 1);
+    if (currentStartItem !== maxUpdateCardCurent) {
+      setCurrentStartItem(currentStartItem + 1);
+    }
   };
 
   const getCardsList = (currentStart: number) => {
@@ -36,21 +41,13 @@ const Slider: React.FC<IProps> = ({ weatherData }) => {
     return (
       <section className='slider-container'>
         <div className="slider-container__btn">
-          {
-            currentStartItem !== 0
-              ? <img className='slider-container__btn-prev' src={arrow} alt='slider arrow' onClick={handleClickPrev} />
-              : null
-          }
+          <img className='slider-container__btn-prev' src={currentStartItem !== 0 ? arrowActive : arrowDisabled} alt='slider arrow' onClick={handleClickPrev} />
         </div>
         <div className='slider-container__card-list'>
           {getCardsList(currentStartItem)}
         </div>
         <div className="slider-container__btn">
-          {
-            currentStartItem !== maxUpdateCardCurent
-              ? <img className='slider-container__btn-next' src={arrow} alt='slider arrow' onClick={handleClickNext} />
-              : null
-          }
+          <img className='slider-container__btn-next' src={currentStartItem !== maxUpdateCardCurent ? arrowActive : arrowDisabled} alt='slider arrow' onClick={handleClickNext} />
         </div>
       </section>
     );
