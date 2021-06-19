be5bc1edbef801e4a6b8a9fda7e8cd18964f988d
Julia Gvozdeva <yulya.gvozdeva.96@mail.ru>
2021-06-20 00:57:25 +0400

    feat: add JSDoc for the filter function

--- src/utils/map.js
+++ src/utils/map.js
@@ -33,8 +33,25 @@ const map = (callback, collection) => {
     }, []);
 };
 
+/**
+ * Function is a predicate, to test each element of the array.
+ * Return a value that coerces to true to keep the element, or to false otherwise.
+ * @callback predicate
+ * @param {T} element - Collection element
+ * @return {boolean} - Callback result item
+ */
+
+/**
+ * The filter() method creates a new array with all elements 
+ * that pass the test implemented by the provided function.
+ * @param {predicate} predicate - Function is a predicate, to test each element of the array.
+ * @param {T[]} collection - Source collection
+ * @returns {T[]} - A new array with each element being the result
+ * of the predicate function.
+ */
+
 const filter = (predicate, collection) => {
-    return collection.reduce((acc, currentValue) => {
+    return collection.reduce(( /** @type {T[]} */ acc, currentValue) => {
         if (predicate(currentValue)) {
             acc.push(currentValue);
         }
