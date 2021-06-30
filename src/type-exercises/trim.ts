36b5040d53ead2834fa8cfe9bfe886e1d070f30e
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-06-30 23:24:09 +0400

    feat: add trim.ts

---
+++ src/type-exercises/trim.ts
@@ -0,0 +1,3 @@
+type WhitespaceCharacter = '\n' | ' ';
+export type Trim<T extends string> = T extends `${WhitespaceCharacter}${infer V}` ? Trim<V> : T extends `${infer V}${WhitespaceCharacter}` ? Trim<V> : T;
+type Trimmed = Trim<'  Hello World  '> // expected to be 'Hello World'
