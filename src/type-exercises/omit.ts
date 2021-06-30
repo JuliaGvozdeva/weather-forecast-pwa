850ffba4db0b2cb2a674e3b4ed8876a749533782
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-06-30 23:29:55 +0400

    fix: eslint errors

--- src/type-exercises/omit.ts
+++ src/type-exercises/omit.ts
@@ -1,13 +1,13 @@
 interface Burger {
-    name: string;
-    numberOfSlices: number;
-    meat: string;
+  name: string;
+  numberOfSlices: number;
+  meat: string;
 }
 
-export type CustomOmit<T extends object, K extends string | number | symbol> = {
-    [P in Exclude<keyof T, K>]: T[P];
-}
+export type CustomOmit<T, K extends string | number | symbol> = {
+  [P in Exclude<keyof T, K>]: T[P];
+};
 
-type VeganBurger = CustomOmit<Burger, 'numberOfSlices' | 'meat'>
+type VeganBurger = CustomOmit<Burger, 'numberOfSlices' | 'meat'>;
 
-type OriginaOmitResult = Omit<Burger, 'numberOfSlices' | 'meat'>
+type OriginaOmitResult = Omit<Burger, 'numberOfSlices' | 'meat'>;
