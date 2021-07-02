7f161e737846c035c5bd4829c9f41802ef9d12c7
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-07-02 19:39:18 +0400

    feat: update typle-to-object.ts

--- src/type-exercises/tuple-to-object.ts
+++ src/type-exercises/tuple-to-object.ts
@@ -1,4 +1,4 @@
-export type TupleToObject<T extends ReadonlyArray<string | number>, V> = Record<T[any], V>;
+export type TupleToObject<T extends ReadonlyArray<string | number>, V> = Record<T[number], V>;
 
 const tuple = ['classic', 'peach breeze', 'pig \'N egg', 'bitburger'] as const;
 type ObjectFromTuple = TupleToObject<typeof tuple, number>;
