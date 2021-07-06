e5a8fa41a54a7a4be9d7f983871ef6854aba01ec
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-07-06 21:59:07 +0400

    feat: add server-workers

--- src/App.tsx
+++ src/App.tsx
@@ -17,4 +17,11 @@ const App: React.FC = () => {
   );
 };
 
+if ('serviceWorker' in navigator) {
+  // Use the window load event to keep the page load performant
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js');
+  });
+}
+
 export default App;
