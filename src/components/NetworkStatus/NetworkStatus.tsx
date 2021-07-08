fe934b7fc0bf516ea609e82ea6fc793b59d20fd2
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-07-08 23:19:24 +0400

    fix: fix network status icon display

--- src/components/NetworkStatus/NetworkStatus.tsx
+++ src/components/NetworkStatus/NetworkStatus.tsx
@@ -3,22 +3,25 @@ import NoNetworkImg from '../../assets/images/icons/cloud-network.png';
 
 const NetworkStatus = () => {
   const [networkStatus, setNetworkStatus] = useState('online');
-  window.addEventListener('load', function () {
-    function updateOnlineStatus(event: any) {
-      const condition = navigator.onLine ? 'online' : 'offline';
-      setNetworkStatus(condition);
-    }
+  // window.addEventListener('load', function () {
+  function updateOnlineStatus(event: any) {
+    const condition = navigator.onLine ? 'online' : 'offline';
+    setNetworkStatus(condition);
+  }
 
-    window.addEventListener('online', updateOnlineStatus);
-    window.addEventListener('offline', updateOnlineStatus);
-  });
+  window.addEventListener('online', updateOnlineStatus);
+  window.addEventListener('offline', updateOnlineStatus);
+  // });
 
   if (networkStatus === 'offline') {
     return (
       <img src={NoNetworkImg} alt="network status" className="no-network-icon" />
     );
   }
-  return <></>;
+
+  return (
+    <img src={NoNetworkImg} alt="network status" className="no-network-icon" hidden />
+  );
 };
 
 export default NetworkStatus;
