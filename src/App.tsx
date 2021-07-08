5fc58225bdfcdebbbd089adb083571c134a62198
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-07-08 14:37:48 +0400

    fix: update project name

--- src/App.tsx
+++ src/App.tsx
@@ -20,7 +20,7 @@ const App: React.FC = () => (
 );
 
 if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/weather-forecast/sw.js').then(function (registration) {
+  navigator.serviceWorker.register('/weather-forecast-pwa/sw.js').then(function (registration) {
     console.log('Service worker registration succeeded:', registration);
   }, function (error) {
     console.log('Service worker registration failed:', error);
