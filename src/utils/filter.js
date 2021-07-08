3446a89930de19c21379c2cca6e9ee010b502d9e
JuliaGvozdeva <yulya.gvozdeva.96@mail.ru>
2021-07-08 12:41:55 +0400

    fix: fix eslint errors and move filter function to the separate file

---
+++ src/utils/filter.js
@@ -0,0 +1,19 @@
+/**
+ * The filter() method creates a new array with all elements 
+ * that pass the test implemented by the provided function.
+ * @param {(currentValue: T) => boolean} predicate - Function is a predicate, to test each element of the array.
+ * @param {T[]} collection - Source collection
+ * @returns {T[]} - A new array with each element being the result
+ * of the predicate function.
+ * @template T
+ */
+
+const filter = (predicate, collection) => {
+    let /** @type {T[]} */ mergedArray = [];
+    return collection.reduce(( /** @type {T[]} */ acc, currentValue) => {
+        if (predicate(currentValue)) {
+            mergedArray = [...acc, currentValue];
+        }
+        return mergedArray;
+    }, []);
+};
--- src/utils/map.js
+++ src/utils/map.js
@@ -14,23 +14,3 @@ const map = (callback, collection) => {
     return [...acc, callback(currentValue)];
   }, []);
 };
-
-/**
- * The filter() method creates a new array with all elements 
- * that pass the test implemented by the provided function.
- * @param {(currentValue: T) => boolean} predicate - Function is a predicate, to test each element of the array.
- * @param {T[]} collection - Source collection
- * @returns {T[]} - A new array with each element being the result
- * of the predicate function.
- * @template T
- */
-
-const filter = (predicate, collection) => {
-  let /** @type {T[]} */ mergedArray = [];
-  return collection.reduce(( /** @type {T[]} */ acc, currentValue) => {
-    if (predicate(currentValue)) {
-      mergedArray = [...acc, currentValue];
-    }
-    return mergedArray;
-  }, []);
-};
